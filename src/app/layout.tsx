5f8f173edcf2071063bfb3d07446cec923d3f880
Wing Sang Yau <shawnyauws@gmail.com>
2025-06-18 04:52:53 +0000

    error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,14 @@ export default function RootLayout({
   return (
     <html lang="zh-HK">
       <head>
+        <meta charSet="utf-8" />
+        {/* Temporarily removed Google Font links for diagnosis */}
+        {/*
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        */}
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
         <Navbar />
